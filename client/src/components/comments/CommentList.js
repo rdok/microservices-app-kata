9c19fa0078165693357cc0fb4a4b534b726eb5e4
Rizart Dokollari <r.dokollari@gmail.com>
2020-08-30 12:05:51 +0100

    Client handles comment pending moderation.

--- client/src/components/comments/CommentList.js
+++ client/src/components/comments/CommentList.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = Object.values(comments).map(comment => {
-    let content = comment.moderationStatus === 'approved'
-      ? comment.content
-      : (<i>This comment has been rejected.</i>);
+    let content;
+    if (comment.moderationStatus === 'approved') {
+      content = comment.content;
+    } else if (comment.moderationStatus === 'pending') {
+      content = (<i>This comment is pending moderation.</i>);
+    } else {
+      content = (<i>This comment has been rejected.</i>);
+    }
 
     return (<li key={comment.id}>{content}</li>);
   });
